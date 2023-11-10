5cb6751b6ab217bad8ae2e3bd9de9548f007b62e
NNE <namoline_760@sfr.fr>
2023-11-10 21:04:40 +0100

    NNE first manipulations

---
+++ src/main/java/com/nnj/batch/DatawarehouseBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nnj.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawarehouseBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatawarehouseBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nnj/batch/DatawarehouseBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnj.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawarehouseBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
